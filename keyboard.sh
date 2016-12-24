d63912bd83fd85e2bd29fc46394d7c675dee3a1c
peterzky <378096232@qq.com>
2016-12-24 15:40:03 +0800

    add xmodmap set key to switch audio output

--- keyboard.sh
+++ keyboard.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sleep 2
-setxkbmap -option ctrl:nocaps -option altwin:swap_alt_win
+# setxkbmap -option ctrl:nocaps -option altwin:swap_alt_win
 # setxkbmap -option ctrl:nocaps 
+xmodmap ~/.Xmodmap
 sleep 1
 xcape -e 'Control_L=Escape'
