ad76197d26a271a22ec81a04bc59fb219ccf6850
peterzky <378096232@qq.com>
2017-01-04 17:06:15 +0800

    add screensaver settings

--- keyboard.sh
+++ keyboard.sh
@@ -2,6 +2,9 @@
 sleep 2
 # setxkbmap -option ctrl:nocaps -option altwin:swap_alt_win
 # setxkbmap -option ctrl:nocaps 
-xmodmap ~/.Xmodmap
+# xmodmap ~/.Xmodmap
 sleep 1
+# start x cape
 xcape -e 'Control_L=Escape'
+# set screensaver to 1 hour
+xset s 3600 3600
